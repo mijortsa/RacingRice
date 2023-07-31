984681ff4f058a88d14312f946276eac2778a880
Azis W Katamsi <astrojim.blankon@gmail.com>
2023-07-31 17:24:19 +0700

    Summery

---
+++ readonly.sh
@@ -0,0 +1,27 @@
+#bin/bash
+names=`ls /Volumes |grep -v Mac`
+base="/Volumes"
+mkdir $base
+tmpFile="/tmp/tmp.txt"
+echo $tmpFile
+sudo echo ""> $tmpFile
+vo=""
+for name in $names
+do
+    vo="/Volumes/$name"
+	if [[ x`diskutil info $vo |grep 'File System Personality'|grep NTFS ` != "x" ]] ; then
+       disk=`diskutil info $vo |grep Node|grep -o "/dev/disk.*"`
+	   echo $name:$disk >> $tmpFile
+	fi
+done
+echo $tmpFile
+for l in `cat $tmpFile`
+do
+	diskutil umount ${l%%:*}
+done
+
+for l in `cat $tmpFile`
+do
+	diskutil mount ${l%%:*}
+done
+open /Volumes
